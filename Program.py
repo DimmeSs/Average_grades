6bd5636d5d9a587cc821446a36b8f74c37370c87
Szymon Słoniowski <karol2016pl@gmail.com>
2023-01-23 15:01:12 +0100

    Initial commit

---
+++ Program.py
@@ -0,0 +1,42 @@
+grades = []
+weights = []
+next_grade = input("#========================================#\n\nJeżeli Chcesz zakończyć -> Wpisz \"end\" \nJeżeli Chcesz korzystać wciśnij [Enter]\n")
+
+while True:
+    while True:
+        grade = input("#========================================#\n\nPodaj ocenę : ")
+        if next_grade.lower() == "end" or grade.lower() == "end": exit()
+        elif grade == "": print("Nie podałeś oceny (Jeżeli chcesz zakończyć program wpisz \"end\"\n")
+        else: break  
+    while True:     
+        weight = input("Podaj wagę oceny : ")
+        try:
+            if next_grade.lower() == "end" or grade.lower() == "end" or weight.lower() == "end":exit()
+            elif weight == "": print("Nie podałeś wagi oceny (Jeżeli chcesz zakończyć program wpisz \"end\"\n\n#========================================#")
+            else:break
+        except AttributeError:
+            break
+    weight = float(weight)
+    if '+' in grade:
+        grade = float(grade.replace('+','')) + 0.5
+    elif '-' in grade:
+        grade = float(grade.replace('-','')) - 0.25
+    else:
+        grade = float(grade)
+        
+    grades.append(grade)
+    weights.append(weight)
+    average = sum([grades[i]*weights[i] for i in range(len(grades))])/sum(weights)
+
+    print("\nTwoja średnia wynosi: [", round(average,2),"]\n")
+    print("|Ocena | Waga| ")
+
+    for a,b in zip(grades,weights):
+        print("[",a,"wagi",int(b),"]")
+    next_grade = input("")
+    try:
+        if next_grade.lower() == "end" or grade.lower() == "end" or weight.lower() == "end":
+            break
+    except AttributeError:
+        pass
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Average_grades
+ program for calculating the average grade using the terminal (there are plans to make it a desktop application)
